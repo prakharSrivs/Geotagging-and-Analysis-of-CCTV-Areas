8798ecd1b7839d9ccb1bfbe172ca1392370a442a
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2024-01-17 18:39:06 +0530

    Final Push

--- web/admin-panel-frontend/src/components/Dashboard/components/AllCameras.js
+++ web/admin-panel-frontend/src/components/Dashboard/components/AllCameras.js
@@ -1,7 +1,8 @@
-import { Box, Button, CircularProgress, Grid, LinearProgress, Typography } from '@mui/material'
-import { MapContainer, Marker, Popup } from 'react-leaflet';
+import { Box, Button, CircularProgress, Dialog, DialogActions, DialogTitle, Grid, LinearProgress, Slider, Typography } from '@mui/material'
+import { MapContainer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import CustomMarkerIcon from '../CustomMarker.png'
 import MyLocationMarker from '../MyLocationMarker.png'
+import SelectedMarker from '../SelectedMarker.png'
 import { TileLayer } from 'react-leaflet';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import L from "leaflet"
@@ -10,6 +11,9 @@ import useGeoLocation from '../hooks/useGeoLocation';
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import TuneIcon from '@mui/icons-material/Tune';
+import CloseIcon from '@mui/icons-material/Close';
+var dist = require('geo-distance-js');
 
 const styles = {
     container:{
@@ -33,6 +37,15 @@ const styles = {
             marginRight:"5px"
         }
     },
+    filtersButton:{
+        position:"absolute",
+        right:"50px",
+        bottom:"100px",
+        zIndex:"1000",
+        "& svg":{
+            marginRight:"5px"
+        }
+    },
     loadingContainer:{
         width:"100%",
         height:"100%",
@@ -40,6 +53,19 @@ const styles = {
         justifyContent:"center",
         alignItems:"center",
         flexDirection:"column",
+    },
+    dialogBodyContainer:{
+        padding:"10px 40px",
+        width:"400px",
+        paddingTop:"0",
+    },
+    dialogCloseIcon:{
+        cursor:"pointer",
+    },
+    dialogHeader:{
+        display:"flex",
+        justifyContent:"space-between",
+        alignItems:"center",
     }
 }
 
@@ -56,6 +82,49 @@ const myLocationMarkerIcon = new L.Icon({
     iconAnchor:[15,40],
     popupAnchor:[0,-47]
 })
+
+const selectedLocationMarkerIcon = new L.Icon({
+    iconUrl: SelectedMarker,
+    iconSize:[28,40.5],
+    iconAnchor:[15,40],
+    popupAnchor:[0,-47]
+})
+
+const FiltersDialog = ({
+    open,
+    setOpen,
+    radius,
+    setRadius,
+    setFilters
+})=>{
+
+    
+
+    return (
+        <Dialog open={open} handleClose={()=>setOpen(false)} >
+        <DialogTitle sx={styles.dialogHeader}>Filters  <CloseIcon sx={styles.dialogCloseIcon} onClick={()=>setOpen(false)} /></DialogTitle>
+            <Box sx={styles.dialogBodyContainer}>
+                <Typography fontFamily={"Mulish"} fontSize={"16px"} ml={"-10px"} mb={"10px"}>
+                    Select a radius in KMs - {radius} {radius!==0 ? (radius==1 ? "KM" : "KMs") : ""}
+                </Typography>
+                <Slider 
+                    color={"secondary"} 
+                    max={10} 
+                    min={0} 
+                    value={radius} 
+                    onChange={(e)=>setRadius(e.target.value)} 
+                    step={1}
+                    defaultValue={0}
+                />
+            </Box>
+        <DialogActions>
+            <Button onClick={setFilters}>
+                Apply Filters
+            </Button>
+        </DialogActions>
+        </Dialog>
+    )
+}
 
 const CustomPopup = ({ownerId})=>{
 
@@ -110,7 +179,6 @@ const CustomPopup = ({ownerId})=>{
 }
 
 const CustomMarker = ({markerCoords,icon,ownerId})=>{
-
     return (
     <Marker 
         position={[markerCoords.lat,markerCoords.lng]}
@@ -119,7 +187,21 @@ const CustomMarker = ({markerCoords,icon,ownerId})=>{
         <CustomPopup ownerId={ownerId} />
     </Marker>
     )
+}
 
+const ClickedPositionMarker = ({markerCoords,icon,ownerId})=>{
+    return (
+        <Marker 
+            position={[markerCoords.lat,markerCoords.lng]}
+            icon={icon}
+        >
+            <Popup>
+                <Typography fontFamily={"Mulish"} fontSize={"16px"} color={"error"}>
+                    Selected Position
+                </Typography>
+            </Popup>
+        </Marker>
+        )
 }
 
 function AllCameras() {
@@ -132,7 +214,23 @@ function AllCameras() {
     const [error,setError] = useState("");
     const [fetching,setFetching] = useState(false);
     const [camerasData,setCamerasData] = useState([]);
+    const [markedPosition,setMarkedPosition] = useState({lat:"",lng:""});
+    const [filteredData,setFilteredData] = useState([]);
+    const [radius,setRadius] = useState(0);
+    const [showFiltersDialog,setShowFilterDialog] = useState(false);
     const navigate = useNavigate();
+
+    const setFilters = ()=>{
+        if(radius===0){
+            setFilteredData([]);
+        }else{
+            const tempFilteredData = camerasData.filter((camera)=>{
+                return calculateDistance(markedPosition,{ lat:camera.latitude, lng:camera.longitude }) < radius ;
+            })
+            setFilteredData(tempFilteredData);
+        }
+        setShowFilterDialog(false);
+    }
 
     const handleLocationFetch = ()=>{
         fetchLocation();
@@ -144,6 +242,52 @@ function AllCameras() {
             );
         }
     }
+
+    const handleFiltersButtonClick = ()=>{
+        if(markedPosition.lat.length && markedPosition.lng.length){
+            if(radius==0){
+                setShowFilterDialog(true);
+            }else{
+                setRadius(0);
+            }
+        }else{
+            alert("First select a location on the map, by clicking anywhere on the map")
+        }
+    }
+
+    const MapEvents = () => {
+        useMapEvents({
+          click(e) {
+            setMarkedPosition({
+                lat:`${e.latlng.lat}`,
+                lng:`${e.latlng.lng}`
+            });
+          },
+        });
+        return false;
+    }
+
+    const calculateDistance = (coord1, coord2) => {
+        const R = 6371; // Earth radius in kilometers
+        const dLat = deg2rad(coord2.lat - coord1.lat);
+        const dLng = deg2rad(coord2.lng - coord1.lng);
+    
+        const a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(deg2rad(coord1.lat)) *
+            Math.cos(deg2rad(coord2.lat)) *
+            Math.sin(dLng / 2) *
+            Math.sin(dLng / 2);
+    
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c; // Distance in kilometers
+    
+        return distance;
+    };
+    
+    const deg2rad = (deg) => {
+        return deg * (Math.PI / 180);
+    };
 
     useEffect(()=>{
 
@@ -208,6 +352,22 @@ function AllCameras() {
                     url={"https://api.maptiler.com/maps/satellite/256/{z}/{x}/{y}.jpg?key=2H9Bu3Li6Uk3CfX4DE79"}
                     attribution={`<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`}
                 />
+                <MapEvents />
+                <FiltersDialog 
+                    setOpen={setShowFilterDialog}
+                    open={showFiltersDialog}
+                    radius={radius}
+                    setRadius={setRadius}
+                    setFilters={setFilters}
+                />
+                {
+                    markedPosition.lat.length>0 && markedPosition.lng.length>0 &&
+                    <ClickedPositionMarker 
+                        markerCoords={markedPosition}
+                        icon={selectedLocationMarkerIcon}
+                        name={" Picked Location "}
+                    />
+                }
                 {
                     location.loaded &&
                     <CustomMarker 
@@ -217,11 +377,30 @@ function AllCameras() {
                     />
                 }
                 {
-                    camerasData.map((data,index)=>{
+                    filteredData.length>0 ?
+                    filteredData.map((data,index)=>{
+                        const coordinates = {
+                            lat:data.latitude,
+                            lng:data.longitude
+                        }
                         return (
                             <CustomMarker 
                                 key={index}
-                                markerCoords={{lat:data.latitude,lng:data.longitude}}
+                                markerCoords={coordinates}
+                                icon={markerIcon}
+                                ownerId={data?.owner_id}
+                            />
+                        )
+                    }) :                    
+                    camerasData.map((data,index)=>{
+                        const coordinates = {
+                            lat:data.latitude,
+                            lng:data.longitude
+                        }
+                        return (
+                            <CustomMarker 
+                                key={index}
+                                markerCoords={coordinates}
                                 icon={markerIcon}
                                 ownerId={data?.owner_id}
                             />
@@ -229,6 +408,18 @@ function AllCameras() {
                     })
                 }
             </MapContainer>
+            <Button 
+                variant={"contained"} 
+                color='secondary' 
+                sx={styles.filtersButton} 
+                p={"10px 40px"} 
+                onClick={handleFiltersButtonClick}
+            >
+                {
+                    radius>0 ? <><CloseIcon/> Clear Filters </> : <><TuneIcon mr={"5px"}/> Filters </>
+                }
+                
+            </Button>
             <Button 
                 variant={"contained"} 
                 color='primary' 
